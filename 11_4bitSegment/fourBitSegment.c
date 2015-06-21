d73602e01a74599b6938a0a865445ae7e5c757db
Jason <adeept@163.com>
2015-06-21 08:28:22 -0700

    add head comment

---
+++ 11_4bitSegment/fourBitSegment.c
@@ -0,0 +1,9 @@
+/*
+* Filename    : fourBitSegment.c
+* Description : display 1,2,3,4...
+* Website     : www.adeept.com
+* E-mail      : support@adeept.com
+* Author      : Jason
+* Date        : 2015/06/21
+*/
+
