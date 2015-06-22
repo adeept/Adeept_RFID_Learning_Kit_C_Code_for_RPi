899ec59d2cd99f842e030f3e224a090e55e71c2d
adeept@163.com <adeept@163.com>
2015-06-22 06:30:57 +0000

    modify function

--- 03_passiveBuzzer/passiveBuzzer.c
+++ 03_passiveBuzzer/passiveBuzzer.c
@@ -4,29 +4,56 @@
 * Website     : www.adeept.com
 * E-mail      : support@adeept.com
 * Author      : Jason
-* Date        : 2015/06/21
+* Date        : 2015/06/22
 */
-#include <wiringPi.h>
+#include <softTone.h>
 #include <stdio.h>
 
-#define BZRPin  0
+#define  BZRPin  0
 
 int main(void)
 {
-	if(wiringPiSetup() == -1){ //when initialize wiringPi failed, print message to screen
-		printf("setup wiringPi failed !\n");
-		return -1; 
+	if(wiringPiSetup() < 0){
+		printf("setup failed !\n");
+		return -1;
 	}
 	
-	pinMode(BZRPin, OUTPUT);   //set pin mode as output mode
-
-	while(1){
-		digitalWrite(BeepPin, LOW);  //beep on
-		delay(100);                  //delay
-		digitalWrite(BeepPin, HIGH); //beep off
-		delay(100);                  //delay
+	if(softToneCreate(BZRPin) < 0){
+		printf("SoftTone setup failed!\n");
+		return -1;
 	}
 
-	return 0;
-}
+	while(1){
+		softToneWrite(BZRPin, 200);	
+		delay(200);
+		softToneWrite(BZRPin, 400);	
+		delay(200);
+		softToneWrite(BZRPin, 600);	
+		delay(200);
+		softToneWrite(BZRPin, 800);	
+		delay(200);
+		softToneWrite(BZRPin, 900);	
+		delay(200);
+		softToneWrite(BZRPin, 1000);	
+		delay(200);
+		softToneWrite(BZRPin, 1100);	
+		delay(200);
+		
+		softToneWrite(BZRPin, 1100);	
+		delay(200);
+		softToneWrite(BZRPin, 1000);	
+		delay(200);
+		softToneWrite(BZRPin, 900);	
+		delay(200);
+		softToneWrite(BZRPin, 800);	
+		delay(200);
+		softToneWrite(BZRPin, 600);	
+		delay(200);
+		softToneWrite(BZRPin, 400);	
+		delay(200);
+		softToneWrite(BZRPin, 200);	
+		delay(200);
+	}
 
+	return 0;	
+}
